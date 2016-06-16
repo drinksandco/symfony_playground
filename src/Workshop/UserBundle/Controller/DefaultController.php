3fa83dd2df5033ddffbe8be0d9a42c1750010e7e
Xavi Serrat <xavi.serrat@uvinum.com>
2016-06-16 10:51:57 +0200

    xavi Add initial UserBundle

---
+++ src/Workshop/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Workshop\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('UserBundle:Default:index.html.twig');
+    }
+}
