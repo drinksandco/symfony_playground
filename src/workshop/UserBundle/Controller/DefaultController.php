d863d265d7e8fe897260a0a8a77c307702e18350
Daniel Madurell <madu2789@hotmail.com>
2016-06-16 11:28:17 +0200

    Create Bundle

---
+++ src/workshop/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace workshop\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('workshopUserBundle:Default:index.html.twig');
+    }
+}
---
+++ src/workshop/UserBundle/workshopUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace workshop\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class workshopUserBundle extends Bundle
+{
+}
