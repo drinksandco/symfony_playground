f007a45bf6b43d62fb0b10bff61f6e1fa46e9ade
Albert Garcia <albert.garcia@uvinum.com>
2016-06-23 14:05:23 +0200

    Use `decorates` and `decorate_inner_name` in services definition YMLs, so we can avoid modifying original decorated components or redefining original service names. Winassu! :jellytime:

--- src/OboBundle/Controller/DefaultController.php
+++ src/OboBundle/Controller/DefaultController.php
@@ -2,18 +2,13 @@
 
 namespace OboBundle\Controller;
 
-use Obokaman\Application\Service\User\AddUser;
-use Obokaman\Application\Service\User\EditUser;
 use Obokaman\Application\Service\User\EditUserRequest;
-use Obokaman\Application\Service\User\ListUsers;
-use Obokaman\Application\Service\User\RemoveUser;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        /** @var ListUsers $app_service */
         $app_service = $this->get('obokaman.application.service.user.list');
 
         $users = $app_service->__invoke();
@@ -25,7 +20,6 @@ class DefaultController extends Controller
 
     public function addAction()
     {
-        /** @var AddUser $app_service */
         $app_service = $this->get('obokaman.application.service.user.add');
         $app_service->__invoke();
 
@@ -34,7 +28,6 @@ class DefaultController extends Controller
 
     public function removeAction($user_id)
     {
-        /** @var RemoveUser $app_service */
         $app_service = $this->get('obokaman.application.service.user.remove');
         $app_service->__invoke($user_id);
 
@@ -43,7 +36,6 @@ class DefaultController extends Controller
 
     public function editAction($user_id, $name, $email)
     {
-        /** @var EditUser $app_service */
         $app_service = $this->get('obokaman.application.service.user.edit');
 
         $request = new EditUserRequest($user_id, $name, $email);
