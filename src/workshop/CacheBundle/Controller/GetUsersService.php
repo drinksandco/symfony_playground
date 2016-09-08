59347c48635eb2ee8ccb4590e3da97414e6f5076
Daniel Madurell <daniel.madurell@verticomm.eu>
2016-09-08 17:45:19 +0200

    Call original Use case

--- src/workshop/CacheBundle/Controller/GetUsersService.php
+++ src/workshop/CacheBundle/Controller/GetUsersService.php
@@ -3,25 +3,29 @@
 namespace workshop\CacheBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use workshop\UserBundle\Controller as User;
 
 class GetUsersService extends Controller
 {
+    /**
+     * @var User\GetUsersService
+     */
+    private $get_users_use_case;
+
     public function __construct()
     {
+        $this->get_users_use_case = new User\GetUsersService();
     }
 
     public function __invoke()
     {
-        $cache = true;
+        $cache = false;
 
         if (true == $cache)
         {
             return $users = ['name' => 'cache!'];
         }
 
-        $get_users_use_case = $this->get('workshop_user.controller.get_users_service');
-        $users              = $get_users_use_case->__invoke();
-
-		return $users;
+        return $users = $this->get_users_use_case->__invoke();
     }
 }
