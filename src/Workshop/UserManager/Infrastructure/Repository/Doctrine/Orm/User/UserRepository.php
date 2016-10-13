7b8e85ef3f21c6f758a83920800587d00acdf752
Xavi Serrat <xavi.serrat@uvinum.com>
2016-10-13 13:58:02 +0200

    xavi: Add first approach to persisting a mapped domain entity using yml and having a 1-N association inside a User.
    This approach doesn't work properly because the skill collection returned by doctrine is a PersistentCollection, so it's impossible to persist a Domain entity using in our domain a SkillCollection and at the same time a PersitentCollection in order to be recognized by doctrine.

--- src/Workshop/UserManager/Infrastructure/Repository/Doctrine/Orm/User/UserRepository.php
+++ src/Workshop/UserManager/Infrastructure/Repository/Doctrine/Orm/User/UserRepository.php
@@ -2,8 +2,11 @@
 
 namespace UserManager\Infrastructure\Repository\Doctrine\Orm\User;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\PersistentCollection;
 use UserManager\Domain\Infrastructure\Repository\User\UserRepository as UserRepositoryContract;
+use UserManager\Domain\Model\User\SkillCollection;
 use UserManager\Domain\Model\User\User;
 use UserManager\Domain\Model\User\UserCollection;
 use UserManager\Domain\Model\User\UserId;
@@ -22,37 +25,37 @@ class UserRepository implements UserRepositoryContract
     {
         $result = $this->entity_manager->getRepository('UserManager:User\User')->findAll();
 
-        $user_collection = new UserCollection();
-
         if (empty($result))
         {
-            return $user_collection;
+            return new UserCollection();
         }
 
-        foreach ($result as $user)
-        {
-            dump($user->skills()->getValues());die; //TODO: Solve this due to currenty it returns a PersistentCollection Object.
-            $user_collection->add($user);
-        }
-
-        dump($user_collection);die;
-
-        return $user_collection;
+        return $this->hydrateUsers($result);
     }
 
     public function findById(UserId $user_id)
     {
-        return $this->entity_manager->getRepository('UserManager:User\User')->find($user_id->userId());
+        $result = $this->entity_manager->getRepository('UserManager:User\User')->find($user_id->userId());
+
+        if (empty($result))
+        {
+            return null;
+        }
+
+        return $this->hydrateUser($result);
     }
 
     public function add(User $a_new_user)
     {
-        $this->entity_manager->persist($a_new_user);
+        $a_doctrine_user = $this->hydrateDoctrineUser($a_new_user);
+        $this->entity_manager->persist($a_doctrine_user);
         $this->entity_manager->flush();
     }
 
     public function update(User $a_user)
     {
+        $a_doctrine_user = $this->hydrateDoctrineUser($a_user);
+        $this->entity_manager->persist($a_doctrine_user);
         $this->entity_manager->flush();
     }
 
@@ -71,4 +74,54 @@ class UserRepository implements UserRepositoryContract
         return true;
     }
 
+    private function hydrateUsers($result)
+    {
+        $user_collection = new UserCollection();
+
+        foreach ($result as $doctrine_user)
+        {
+            $user = $this->hydrateUser($doctrine_user);
+
+
+            $user_collection->add($user);
+        }
+
+        return $user_collection;
+    }
+
+    private function hydrateUser(User $doctrine_user)
+    {
+        return new User(
+            $doctrine_user->userId(),
+            $doctrine_user->name(),
+            $doctrine_user->surname(),
+            $doctrine_user->username(),
+            $doctrine_user->email(),
+            $this->hydrateSkillsCollection($doctrine_user->skills())
+        );
+    }
+
+    private function hydrateDoctrineUser(User $user)
+    {
+        return new User(
+            $user->userId(),
+            $user->name(),
+            $user->surname(),
+            $user->username(),
+            $user->email(),
+            new ArrayCollection($user->skills()->skills())
+        );
+    }
+
+    private function hydrateSkillsCollection(PersistentCollection $skills)
+    {
+        $skills_collection = new SkillCollection();
+
+        foreach ($skills as $skill)
+        {
+            $skills_collection->add($skill);
+        }
+
+        return $skills_collection;
+    }
 }
