b332dc39af939f27e9f0abe91d4e105300a61654
Marcos <velozmarkdrea@gmail.com>
2017-04-21 11:26:07 +0200

    2. Etag hash based

--- src/Playground/MainBundle/Controller/DefaultController.php
+++ src/Playground/MainBundle/Controller/DefaultController.php
@@ -11,11 +11,15 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $response = new Response();
-        $response->setPublic();
-        $response->setSharedMaxAge(10);
 
         $app_service = $this->get('playground.application.service.user.list');
         $users_list  = $app_service->__invoke();
+
+        $response->setEtag(md5(serialize($users_list)));
+        if($response->isNotModified($request))
+        {
+            return $response;
+        }
 
         return $this->render('MainBundle:Default:index.html.twig', ['users' => $users_list], $response);
     }
