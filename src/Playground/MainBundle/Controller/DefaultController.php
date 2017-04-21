1360aac1976c8ab34e2a3d98ed766afa108967cd
Daniel Madurell <madu2789@gmail.com>
2017-04-21 11:28:24 +0200

    Ordering

--- src/Playground/MainBundle/Controller/DefaultController.php
+++ src/Playground/MainBundle/Controller/DefaultController.php
@@ -13,17 +13,21 @@ class DefaultController extends Controller
         $response = new Response();
         $response->setPrivate();
 
-        // cache for 10 seconds
-        $response->setSharedMaxAge(10);
+        $app_service = $this->get('playground.application.service.user.list');
+        $users_list  = $app_service->__invoke();
 
         // (optional) set a custom Cache-Control directive
         $response->headers->addCacheControlDirective('must-revalidate', true);
 
-        $app_service = $this->get('playground.application.service.user.list');
-        $users_list  = $app_service->__invoke();
+        // 1 - Public cache
+        $response->setSharedMaxAge(10);
 
+        // 2 - Etag cache
         $hash_content = serialize($users_list);
         $response->setEtag(md5($hash_content));
+
+        // 3 - Last Modified cache
+        //...
 
         return $this->render('MainBundle:Default:index.html.twig', ['users' => $users_list], $response);
     }
